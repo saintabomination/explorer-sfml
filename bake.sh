64ccde516bc4c0bbb3964a82a7f2cd2abde62919
saintabomination <saint.abomination@protonmail.com>
2022-05-10 21:24:21 +0200

    Adding SFML

--- bake.sh
+++ bake.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PROJECT_NAME="explorer-sfml"
+LIBRARIES="-lsfml-graphics -lsfml-window -lsfml-system"
 
 g++ -c src/Main.cpp
-g++ *.o -o build/$PROJECT_NAME
+g++ *.o -o build/$PROJECT_NAME $LIBRARIES
 rm -rf *.o
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,32 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-  std::cout << "Initial Commit!" << std::endl;
+  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
+  sf::RectangleShape shape(sf::Vector2f(20.f, 40.f));
+  shape.setFillColor(sf::Color::Red);
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      switch (event.type)
+      {
+        case sf::Event::Closed:
+          window.close();
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    window.clear();
+    window.draw(shape);
+    window.display();
+  }
+
   return 0;
 }
