88f90cf55d3392ba90cce1458bfb51ca8b793838
saintabomination <saint.abomination@protonmail.com>
2022-05-13 19:53:44 +0200

    Creating Game class

--- bake.sh
+++ bake.sh
@@ -3,6 +3,7 @@
 PROJECT_NAME="explorer-sfml"
 LIBRARIES="-lsfml-graphics -lsfml-window -lsfml-system"
 
-g++ -c src/Main.cpp
-g++ *.o -o build/$PROJECT_NAME $LIBRARIES
+g++ -c src/Main.cpp src/Classes/*.cpp
+mkdir -p build
+g++ *.o -o build/$PROJECT_NAME -I src/Headers $LIBRARIES
 rm -rf *.o
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,32 +1,9 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
+#include "Headers/Game.h"
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML");
-  sf::RectangleShape shape(sf::Vector2f(20.f, 40.f));
-  shape.setFillColor(sf::Color::Red);
-
-  while (window.isOpen())
-  {
-    sf::Event event;
-    while (window.pollEvent(event))
-    {
-      switch (event.type)
-      {
-        case sf::Event::Closed:
-          window.close();
-          break;
-
-        default:
-          break;
-      }
-    }
-
-    window.clear();
-    window.draw(shape);
-    window.display();
-  }
+  Game game;
+  game.run();
 
   return 0;
 }
